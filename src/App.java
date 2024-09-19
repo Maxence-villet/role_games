aa0de7176c44da334f1f87934b2d1543e3d49a3b
Maxence.villet <137928220+Maxence-villet@users.noreply.github.com>
2024-09-19 18:55:24 +0200

    Update App.java

--- src/App.java
+++ src/App.java
@@ -29,7 +29,7 @@ public class App {
                 if ("YES".equals(choice)) {
                     turn = true;
                 }
-                
+                //oui
             }
             
             turn = false;
